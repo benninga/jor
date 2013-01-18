46d809592a1e8e6377bd26b223069838877aa1b7
Amos Benninga <joecats41-github@benninga.org>
2013-01-18 11:35:51 -0500

    Added insiders report for Sales team.

---
+++ src/java/server/org/jor/rest/report/report/sales/insiders_report.sql
@@ -0,0 +1,5 @@
+SELECT m.email, to_timestamp(m.member_created_at) AS member_creation_date,
+       to_timestamp(m.access_granted_at) AS insider_since,
+       to_timestamp(MAX(e.event_time)) AS last_activity_at
+FROM events e INNER JOIN member_dimension m ON (m.id = e.member_id AND m.has_private_projects = true)
+GROUP BY m.email, m.member_created_at, m.access_granted_at
