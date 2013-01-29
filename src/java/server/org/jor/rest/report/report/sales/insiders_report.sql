0f5cba949135e7fa2bf61ebd2656e864fda85f31
Amos Benninga <joecats41-github@benninga.org>
2013-01-28 21:34:47 -0500

    Show all insiders, even those without recent activity on private projects.

--- src/java/server/org/jor/rest/report/report/sales/insiders_report.sql
+++ src/java/server/org/jor/rest/report/report/sales/insiders_report.sql
@@ -1,7 +1,11 @@
 SELECT m.email, to_timestamp(m.member_created_at) AS member_creation_date,
        to_timestamp(m.access_granted_at) AS insider_since,
        to_timestamp(MAX(e.event_time)) AS last_activity_at
-FROM events e
-     INNER JOIN member_dimension m ON (m.id = e.member_id AND m.has_private_projects = true)
-     INNER JOIN project_dimension p ON (p.id = e.project_id AND p.is_private)
+FROM member_dimension m
+     LEFT OUTER JOIN
+     (
+      SELECT e.event_time, e.member_id
+      FROM events e INNER JOIN project_dimension p ON (p.id = e.project_id AND p.is_private)
+     ) e ON (m.id = e.member_id AND m.has_private_projects = true)
+WHERE m.has_private_projects = true
 GROUP BY m.email, m.member_created_at, m.access_granted_at
